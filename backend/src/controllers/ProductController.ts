6acf6900dbc902ce8d580fab8f9b09a61c011436
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-09-14 18:47:30 +0200

    fix: type of params/ product controller

--- backend/src/controllers/ProductController.ts
+++ backend/src/controllers/ProductController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
+import mongoose from "mongoose";
 import { Product } from "../models";
 
 export const getProductsByShop = async (req: Request, res: Response) => {
   try {
-    const products = await Product.find({ shopId: req.params.id });
+    const products = await Product.find({
+      shopId: new mongoose.Types.ObjectId(req.params.id),
+    });
     res.json(products);
   } catch (err) {
     res.status(500).json({ message: "Server error" });
