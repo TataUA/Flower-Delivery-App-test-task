161b12ac8924682dbae51913b750ada25d9b9c1c
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-09-15 20:30:37 +0200

    fix: delete user creation

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,24 +1,6 @@
 import { Request, Response } from "express";
 import { User } from "../models";
 
-export const createUser = async (req: Request, res: Response) => {
-  try {
-    const { name, email, phone, address } = req.body;
-    if (!name || !email || !phone || !address) {
-      return res.status(400).json({ message: "Missed required field" });
-    }
-
-    let user = await User.findOne({ email, phone });
-    if (!user) {
-      user = await User.create({ name, email, phone, address });
-    }
-
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
 export const getUser = async (req: Request, res: Response) => {
   try {
     const { email, phone } = req.body;
