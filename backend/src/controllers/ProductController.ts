9a72df54f3e9c7a387f25b99e213664fe948c95e
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-09-15 10:10:09 +0200

    feat: add endpoint products by id

--- backend/src/controllers/ProductController.ts
+++ backend/src/controllers/ProductController.ts
@@ -12,3 +12,26 @@ export const getProductsByShop = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const getProducts = async (req: Request, res: Response) => {
+  try {
+    const { productIds } = req.body;
+
+    if (!Array.isArray(productIds) || productIds.length === 0) {
+      return res.status(400).json({ message: "Product IDs are required" });
+    }
+
+    const objectIds = productIds.map(
+      (id: string) => new mongoose.Types.ObjectId(id)
+    );
+
+    const products = await Product.find({
+      _id: {
+        $in: objectIds,
+      },
+    });
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
