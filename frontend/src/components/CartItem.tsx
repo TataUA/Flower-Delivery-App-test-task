ef1462afb2fd6cb1d851f75142ae1a0479fde805
Tetiana Khyzhniak <khyzhnjaktetjana@gmail.com>
2025-09-15 15:27:17 +0200

    fix: import

--- frontend/src/components/CartItem.tsx
+++ frontend/src/components/CartItem.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import { useStore } from "@/services/state";
-import { IProductWithQuantity } from "./CartList";
+import { IProductWithQuantity } from "./CartForm";
 
 interface CartItemProps {
   product: IProductWithQuantity;
